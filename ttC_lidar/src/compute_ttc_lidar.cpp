b8944cd5384db4cf7be1f990e15dfde85c0c6b25
Mahesh Kumar Karikalan <mahe.kumar@hotmail.com>
2020-05-08 10:22:13 +0200

    Add lane width check to compute time to collision(ttc) from lidar

--- ttC_lidar/src/compute_ttc_lidar.cpp
+++ ttC_lidar/src/compute_ttc_lidar.cpp
@@ -2,7 +2,6 @@
 #include <numeric>
 #include <opencv2/core.hpp>
 
-
 #include "dataStructures.h"
 #include "structIO.hpp"
 
@@ -12,19 +11,21 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double &TTC)
 {
     // auxiliary variables
-    double dT = 0.1;        // time between two measurements in seconds
-    double laneWidth = 4.0; // assumed width of the ego lane
+    double dT = 0.1;              // time between two measurements in seconds
+    double laneWidth = 4.0 / 100; // assumed width of the ego lane
 
     // find closest distance to Lidar points within ego lane
     double minXPrev = 1e9, minXCurr = 1e9;
     for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
     {
-        minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        if (fabs(it->y) <= laneWidth / 2)
+            minXPrev = minXPrev > it->x ? it->x : minXPrev;
     }
 
     for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
     {
-        minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        if (fabs(it->y) <= laneWidth / 2)
+            minXCurr = minXCurr > it->x ? it->x : minXCurr;
     }
 
     // compute TTC from both measurements
@@ -37,7 +38,6 @@ int main()
     std::vector<LidarPoint> currLidarPts, prevLidarPts;
     readLidarPts("../dat/C22A5_currLidarPts.dat", currLidarPts);
     readLidarPts("../dat/C22A5_prevLidarPts.dat", prevLidarPts);
-
 
     double ttc;
     computeTTCLidar(prevLidarPts, currLidarPts, ttc);
