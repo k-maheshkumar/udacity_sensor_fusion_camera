b5543e4247bab0b83bcdc26a6f40de4c908b36bb
Mahesh Kumar Karikalan <mahe.kumar@hotmail.com>
2020-05-09 14:35:50 +0200

    Add Gaussian blur solution

--- exercises/gradient_filtering/src/gaussian_smoothing.cpp
+++ exercises/gradient_filtering/src/gaussian_smoothing.cpp
@@ -12,20 +12,25 @@ void gaussianSmoothing1()
     cv::Mat img;
     img = cv::imread("../images/img1gray.png");
 
+    // show input
+    string windowName = "input";
+    cv::namedWindow(windowName, 1); // create window
+    cv::imshow(windowName, img);
+
     // create filter kernel
     float gauss_data[25] = {1, 4, 7, 4, 1,
                             4, 16, 26, 16, 4,
                             7, 26, 41, 26, 7,
                             4, 16, 26, 16, 4,
                             1, 4, 7, 4, 1};
-    cv::Mat kernel = cv::Mat(5, 5, CV_32F, gauss_data);
+    cv::Mat kernel = (cv::Mat(5, 5, CV_32F, gauss_data)) / 273.0;
 
     // apply filter
     cv::Mat result;
     cv::filter2D(img, result, -1, kernel, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
 
     // show result
-    string windowName = "Gaussian Blurring";
+    windowName = "Gaussian Blurring";
     cv::namedWindow(windowName, 1); // create window
     cv::imshow(windowName, result);
     cv::waitKey(0); // wait for keyboard input before continuing
