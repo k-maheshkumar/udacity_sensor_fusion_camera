b565f6eaef91df1acecf7dc5c05110cccebe977d
Mahesh Kumar Karikalan <mahe.kumar@hotmail.com>
2020-05-10 11:38:05 +0200

    Make SIFT work ---> # OpenCV 4.1.2 installed from source

--- exercises/describe_keypoints/src/describe_keypoints.cpp
+++ exercises/describe_keypoints/src/describe_keypoints.cpp
@@ -4,8 +4,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
-// #include <opencv2/xfeatures2d.hpp>
-// #include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
 
 using namespace std;
 
@@ -38,34 +38,33 @@ void descKeypoints1()
     string windowName = "BRISK Results";
     cv::namedWindow(windowName, 1);
     imshow(windowName, visImage);
-    cv::waitKey(0);
 
     // TODO: Add the SIFT detector / descriptor, compute the
     // time for both steps and compare both BRISK and SIFT
     // with regard to processing speed and the number and
     // visual appearance of keypoints.
 
-    // detector = cv::xfeatures2d::SIFT::create();
-    // vector<cv::KeyPoint> kptsSIFT;
+    detector = cv::xfeatures2d::SIFT::create();
+    vector<cv::KeyPoint> kptsSIFT;
 
-    // t = (double)cv::getTickCount();
-    // detector->detect(imgGray, kptsSIFT);
-    // t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-    // cout << "SIFT detector with n= " << kptsSIFT.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+    t = (double)cv::getTickCount();
+    detector->detect(imgGray, kptsSIFT);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "SIFT detector with n= " << kptsSIFT.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
 
-    // descriptor = cv::xfeatures2d::SiftDescriptorExtractor::create();
-    // cv::Mat descSIFT;
-    // t = (double)cv::getTickCount();
-    // descriptor->compute(imgGray, kptsSIFT, descSIFT);
-    // t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-    // cout << "SIFT descriptor in " << 1000 * t / 1.0 << " ms" << endl;
+    descriptor = cv::xfeatures2d::SiftDescriptorExtractor::create();
+    cv::Mat descSIFT;
+    t = (double)cv::getTickCount();
+    descriptor->compute(imgGray, kptsSIFT, descSIFT);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "SIFT descriptor in " << 1000 * t / 1.0 << " ms" << endl;
 
-    // visImage = img.clone();
-    // cv::drawKeypoints(img, kptsSIFT, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    // windowName = "SIFT Results";
-    // cv::namedWindow(windowName, 2);
-    // imshow(windowName, visImage);
-    // cv::waitKey(0);
+    visImage = img.clone();
+    cv::drawKeypoints(img, kptsSIFT, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    windowName = "SIFT Results";
+    cv::namedWindow(windowName, 2);
+    imshow(windowName, visImage);
+    cv::waitKey(0);
 }
 
 int main()
